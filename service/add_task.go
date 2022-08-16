28c21fc91f1e9364e618216ae75ab8f33cda475a
itict10 <itict10@gmail.com>
2022-08-16 20:42:02 +0900

    [ADD] 認証保護エンドポイント

--- service/add_task.go
+++ service/add_task.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/itoi10/go-webapp/auth"
 	"github.com/itoi10/go-webapp/entity"
 	"github.com/itoi10/go-webapp/store"
 )
@@ -13,12 +14,21 @@ type AddTask struct {
 	Repo TaskAdder
 }
 
-// 実装
+// タスク追加
 func (a *AddTask) AddTask(ctx context.Context, title string) (*entity.Task, error) {
+	// アクセストークンからユーザID取得
+	id, ok := auth.GetUserID(ctx)
+	if !ok {
+		// エラーメッセージ設定 （JSONでエラーレスポンスを返すのはHandlerの役割）
+		return nil, fmt.Errorf("useer_id not found")
+	}
+	// ユーザID, タイトル, ステータス 設定
 	t := &entity.Task{
+		UserID: id,
 		Title:  title,
 		Status: entity.TaskStatusTodo,
 	}
+	// 登録
 	err := a.Repo.AddTask(ctx, a.DB, t)
 	if err != nil {
 		return nil, fmt.Errorf("failed to register: %w", err)
--- service/list_task.go
+++ service/list_task.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/itoi10/go-webapp/auth"
 	"github.com/itoi10/go-webapp/entity"
 	"github.com/itoi10/go-webapp/store"
 )
@@ -16,7 +17,12 @@ type ListTask struct {
 }
 
 func (l *ListTask) ListTasks(ctx context.Context) (entity.Tasks, error) {
-	ts, err := l.Repo.ListTasks(ctx, l.DB)
+	// アクセストークンからユーザID取得
+	id, ok := auth.GetUserID(ctx)
+	if !ok {
+		return nil, fmt.Errorf("user_id not found")
+	}
+	ts, err := l.Repo.ListTasks(ctx, l.DB, id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to list: %w", err)
 	}
